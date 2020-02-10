4977cb504c2ca790aa2a6704b06cf8dd01622fa7
Tayler Stoppel <stoppel@users.campus>
2020-02-10 17:00:33 -0600

    Added the Drink base class as well as drink tests from upstream

---
+++ Data/Drink.cs
@@ -0,0 +1,42 @@
+﻿/* 
+ * Author: Tayler Stoppel
+ * Class: Drink.cs
+ * Purpose: An abstract base class for all drinks
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// The base class for all drinks.
+    /// </summary>
+    public abstract class Drink
+    {
+        /// <summary>
+        /// Price of the Drink.
+        /// </summary>
+        public virtual double Price { get; set; }
+
+        /// <summary>
+        /// Calories of the Drink.
+        /// </summary>
+        public virtual uint Calories { get; set; }
+
+        /// <summary>
+        /// Ingredients in the Drink.
+        /// </summary>
+        public virtual List<string> Ingredients { get; set; }
+
+        /// <summary>
+        /// Size of the Drink
+        /// </summary>
+        public virtual Size Size { get; set; }
+
+        /// <summary>
+        /// If Drink has ice.
+        /// </summary>
+        public virtual bool Ice { get; set; }
+    }
+}
